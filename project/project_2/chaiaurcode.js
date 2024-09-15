f0a8e6b08779eeffaa680ba5c4db9b91b6b4171a
xcj7 <xcjtushar@gmail.com>
2024-09-15 17:32:22 +0600

    project 4

--- project/project_2/chaiaurcode.js
+++ project/project_2/chaiaurcode.js
@@ -5,14 +5,25 @@ form.addEventListener('submit',function(e){
     console.log("calculate button clicked !")
     console.log(e)
 
-    // let height =  document.getElementById('height').value
-    // let weight =  parseInt(document.querySelector('#weight').value)
-    // let results =  document.querySelector('#results').value
+    let height =  document.getElementById('height').value
+    let weight =  parseInt(document.querySelector('#Weight').value)
+    let results =  document.querySelector('#results')
 
-    // console.log(`height is ${height}`)
+    console.log(`height is ${height}`)
+    console.log(`weight is ${weight}`)
 
-    // if(height === '' || height < 0 || isNaN(height))
-    // {
-    //     results.innerHTML = "Please give a valid height ";
-    // }
+    if(height === '' || height < 0 || isNaN(height))
+    {
+        results.innerHTML = "Please give a valid height ";
+    }
+
+    else if(weight === '' || weight < 0 || isNaN(weight))
+        {
+            results.innerHTML = "Please give a valid weight ";
+        }
+
+    else {
+        let bmi = (weight / ((height*height)/10000)).toFixed(2)
+        results.innerHTML=`<span>${bmi}</span>`;
+    }
 });
